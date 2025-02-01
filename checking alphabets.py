618707f3d0f3a96387aeebf90762ad71dd54f7f1
Abir <abirgpx@gmail.com>
2025-02-01 16:00:33 +0600

    hi

--- checking alphabets.py
+++ checking alphabets.py
@@ -1,7 +1,10 @@
 c = input("Enter a Letter: ")
 
 if c >= "a" and c <= "z":
-    print(f"The character {c} is a LOWERCASE alphabet")
+    print(f"The character {c} is a LOWERCASE Alphabet")
+    
+elif c >= "A" and c <="Z":
+    print(f"The character {c} is a UPPERCASE Alphabet")
     
 else:
-    print(f"The character {c} is NOT an alphabet")
+    print(f"The character {c} is NOT an Alphabet")
